1c73238d3b1b6f46a910bcd90492322174301d0d
rohit123-start <rohitlgajam123@gmail.com>
2024-02-09 19:32:57 +0530

    added socket for users

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const __dirname = "D:/Express_App/public"
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (msg) => {
+    console.log('Message received:', msg);
+    // Broadcast the message to all other connected clients
+    io.emit('message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
