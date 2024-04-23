76edb0c6c1f8b16fae3c72bdbee76e57c81c3f6b
rohit123-start <rohitlgajam123@gmail.com>
2024-04-23 19:46:18 +0530

    commit made

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(imageRoutes)
 const server = http.createServer(app);
 const io = new Server(server);
 
+console.log("hii")
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
